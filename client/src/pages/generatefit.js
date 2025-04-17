407186d0354327b29486c2027beef8e0faf44c9b
feebeeabbru <109037157+feebeeabbru@users.noreply.github.com>
2025-04-17 15:28:51 -0400

    Update generatefit.js

--- client/src/pages/generatefit.js
+++ client/src/pages/generatefit.js
@@ -1,14 +1,58 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import './generatefit.css';
 
 function GenerateFit() {
+	const [ratings, setRatings] = useState([1, 1, 1]);
+	const labels = ["Color", "Weather", "Fit"];
+
+	const handleRatingChange = (index, value) => {
+		const updated = [...ratings];
+		updated[index] = parseInt(value);
+		setRatings(updated);
+	};
 
 	return (
-		<div>
-			<p>This page generates the fit</p>
+		<div className="max-w-3xl mx-auto p-4">
+			<div className="image-grid">
+				<img src="/snoop.jpg" alt="outerwear" className="image-item" />
+				<img src="/snoop.jpg" alt="top" className="image-item" />
+				<img src="/snoop.jpg" alt="bottom" className="image-item" />
+				<img src="/snoop.jpg" alt="shoes" className="image-item" />
+			</div>
+
+			<div className="lower-buttons">
+				<div className="radio-group2 ">
+					<button className="backbutton">
+						Regenerate
+					</button>
+
+					<div className="flex gap-4">
+						{ratings.map((rating, i) => (
+							<select
+								key={i}
+								className="border rounded px-2 py-1"
+								value={rating}
+								onChange={(e) => handleRatingChange(i, e.target.value)}
+							>
+								{[1, 2, 3, 4, 5].map((num) => (
+									<option key={num} value={num}>{num}</option>
+								))}
+							</select>
+						))}
+					</div>
+
+					{/* Right button */}
+					<Link to="/">
+
+						<button className="backbutton">
+							Love it!
+						</button>
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
-
 }
 
 export default GenerateFit; 
